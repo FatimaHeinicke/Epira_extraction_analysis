1a5371a07dc2d934e593005931a78902f0dfc009
fatimaheinicke <fatima.heinicke@studmed.uio.no>
2016-07-26 10:08:04 +0200

    Created plots for CD4naive, CD4mem and CD19 that display an overview of the concentrations of the samples, which samples reach 100ng/6ul, samples with 260/280< 1.85, mean and min concentration

--- Overview_extraction.R
+++ Overview_extraction.R
@@ -99,6 +99,62 @@ legend("topleft", legend= c("Sample with 260/280 < 1.85",
        pt.cex= 1.2, x.intersp= 0.5, y.intersp= 0.7 )
 dev.off()
 
+
+
+
+###Plot concentration of samples
+Tot_NanoDrop_CD4naive2 <- read.table(paste(path, "Extraction/NanoDrop_totalRNA/NanoDrop_CD4naive.csv",
+                                          sep=""), dec=",", header = T, sep=";",
+                                    stringsAsFactors = F)
+#If I would wish to sort the samples
+#plot(sort(Tot_NanoDrop_CD4naive2$ng_ul), pch= 20, ylim= c(0,130))
+#text(sort(Tot_NanoDrop_CD4naive2$ng_ul), labels= Tot_NanoDrop_CD4naive2$SampleID[order(
+#  Tot_NanoDrop_CD4naive2$ng_ul)],
+#     srt=90, cex= 0.8, pos= 3, offset= 0.8)
+
+# Mark those samples that reach a concentration above 100ng in 6 ul because this is the
+# maximum input amount in the NebNext kit
+# Create a new column with the concentrations times 6ul
+Tot_NanoDrop_CD4naive2$conc_at_6_ul <- 6* Tot_NanoDrop_CD4naive2$ng_ul 
+
+path2 <- "/Users/fatimahe/Documents/Research/Epira/Lab/Extraction/Overview_analysis_in_R/Output_from_R_scripts/"
+pdf(paste(path2, "Output_overview_extraction/CD4naive/CD4naiveConcentration.pdf", sep=""),
+    width = 30/2.54, height= 21/2.54)
+plot(Tot_NanoDrop_CD4naive2$ng_ul, pch= 20, ylim= c(0,140), xlab= "SampleID", 
+     ylab= "Concentration in ng/ul", main= "Concentration CD4 naive")
+text(Tot_NanoDrop_CD4naive2$ng_ul, labels= Tot_NanoDrop_CD4naive2$SampleID,
+  srt=90, cex= 0.6, pos= 3, offset= 1.7, 
+  col= ifelse(Tot_NanoDrop_CD4naive2$conc_at_6_ul < 100, 2, 1))
+
+text(Tot_NanoDrop_CD4naive2$ng_ul[Tot_NanoDrop_CD4naive2$X260_280< 1.85] ~ 
+       row.names(Tot_NanoDrop_CD4naive2)[Tot_NanoDrop_CD4naive2$X260_280< 1.85],
+     labels= Tot_NanoDrop_CD4naive2$X260_280[Tot_NanoDrop_CD4naive2$X260_280< 1.85],
+     col= 4,  srt=90, cex= 0.6, pos= 1, offset= 0.8)
+
+text(Tot_NanoDrop_CD4naive2$ng_ul[Tot_NanoDrop_CD4naive2$conc_at_6_ul< 100] ~ 
+       row.names(Tot_NanoDrop_CD4naive2)[Tot_NanoDrop_CD4naive2$conc_at_6_ul< 100],
+     labels= Tot_NanoDrop_CD4naive2$X260_280[Tot_NanoDrop_CD4naive2$conc_at_6_ul< 100],
+     col= 4,  srt=90, cex= 0.6, pos= 1, offset= 0.8)
+
+abline(h= mean(Tot_NanoDrop_CD4naive2$ng_ul), col = "orange", lty = 4)
+# This line of code do the same as the line above:
+#abline(a= mean(Tot_NanoDrop_Cd4naive2$ng_ul), b= 0, col = 2)
+abline(a= 20, b= 0, col= "darkorange", lty =5)
+
+#Legend
+legend(x= -1, y= 145, legend= c("Sample with less than 100ng/6ul ",
+                            "Sample with more than 100ng/6ul",
+                            "260/280 value",
+                            "Mean concentration",
+                            "Min concentration for 100ng in 6ul sample"), 
+       pch= c(NA, NA, NA, NA, NA), lty= c(1, 1,1 ,4 , 5), 
+       col= c(2,1,4,"orange","darkorange"), cex= 0.8, bty= "n", 
+       pt.cex= 1.2, x.intersp= 0.5, y.intersp= 0.7 )
+
+dev.off()
+
+
+
 #nrow(Tot_NanoDrop_CD4naive)
 #Get an overview of the samples included in dataframe (get an indication of
 #samples that are missing)
@@ -175,7 +231,7 @@ dev.off()
 sort(Tot_BioAnalyzer_CD4naive$SampleID)
 
 
-########################## CD4 memeory  ################################# 
+########################## CD4 memory  ################################# 
 ########################## NanoDrop
 
 Tot_NanoDrop_CD4mem <- read.table(paste(path, "Extraction/NanoDrop_totalRNA/NanoDrop_CD4mem.csv",
@@ -267,6 +323,60 @@ dev.off()
 
 
 
+
+###Plot concentration of samples
+Tot_NanoDrop_CD4mem2 <- read.table(paste(path, "Extraction/NanoDrop_totalRNA/NanoDrop_CD4mem.csv",
+                                           sep=""), dec=",", header = T, sep=";",
+                                     stringsAsFactors = F)
+#If I would wish to sort the samples
+#plot(sort(Tot_NanoDrop_CD4naive2$ng_ul), pch= 20, ylim= c(0,130))
+#text(sort(Tot_NanoDrop_CD4naive2$ng_ul), labels= Tot_NanoDrop_CD4naive2$SampleID[order(
+#  Tot_NanoDrop_CD4naive2$ng_ul)],
+#     srt=90, cex= 0.8, pos= 3, offset= 0.8)
+
+# Mark those samples that reach a concentration above 100ng in 6 ul because this is the
+# maximum input amount in the NebNext kit
+# Create a new column with the concentrations times 6ul
+Tot_NanoDrop_CD4mem2$conc_at_6_ul <- 6* Tot_NanoDrop_CD4mem2$ng_ul 
+
+path2 <- "/Users/fatimahe/Documents/Research/Epira/Lab/Extraction/Overview_analysis_in_R/Output_from_R_scripts/"
+pdf(paste(path2, "Output_overview_extraction/CD4memory/CD4memoryConcentration.pdf", sep=""),
+    width = 30/2.54, height= 21/2.54)
+plot(Tot_NanoDrop_CD4mem2$ng_ul, pch= 20, ylim= c(0,170), xlab= "SampleID", 
+     ylab= "Concentration in ng/ul", main= "Concentration CD4 memory")
+text(Tot_NanoDrop_CD4mem2$ng_ul, labels= Tot_NanoDrop_CD4mem2$SampleID,
+     srt=90, cex= 0.6, pos= 3, offset= 1.7, 
+     col= ifelse(Tot_NanoDrop_CD4mem2$conc_at_6_ul < 100, 2, 1))
+
+text(Tot_NanoDrop_CD4mem2$ng_ul[Tot_NanoDrop_CD4mem2$X260_280< 1.85] ~ 
+       row.names(Tot_NanoDrop_CD4mem2)[Tot_NanoDrop_CD4mem2$X260_280< 1.85],
+     labels= Tot_NanoDrop_CD4mem2$X260_280[Tot_NanoDrop_CD4mem2$X260_280< 1.85],
+     col= 4,  srt=90, cex= 0.6, pos= 1, offset= 0.8)
+
+text(Tot_NanoDrop_CD4mem2$ng_ul[Tot_NanoDrop_CD4mem2$conc_at_6_ul< 100] ~ 
+       row.names(Tot_NanoDrop_CD4mem2)[Tot_NanoDrop_CD4mem2$conc_at_6_ul< 100],
+     labels= Tot_NanoDrop_CD4mem2$X260_280[Tot_NanoDrop_CD4mem2$conc_at_6_ul< 100],
+     col= 4,  srt=90, cex= 0.6, pos= 1, offset= 0.8)
+
+abline(h= mean(Tot_NanoDrop_CD4mem2$ng_ul), col = "orange", lty = 4)
+# This line of code do the same as the line above:
+#abline(a= mean(Tot_NanoDrop_Cd4naive2$ng_ul), b= 0, col = 2)
+abline(a= 20, b= 0, col= "darkorange", lty =5)
+
+#Legend
+legend(x= 38, y= 175, legend= c("Sample with less than 100ng/6ul ",
+                                "Sample with more than 100ng/6ul",
+                                "260/280 value",
+                                "Mean concentration",
+                                "Min concentration for 100ng in 6ul sample"), 
+       pch= c(NA, NA, NA, NA, NA), lty= c(1, 1,1 ,4 , 5), 
+       col= c(2,1,4,"orange","darkorange"), cex= 0.8, bty= "n", 
+       pt.cex= 1.2, x.intersp= 0.5, y.intersp= 0.7 )
+
+dev.off()
+
+
+
 ########################## BioAnalyzer
 Tot_BioAnalyzer_CD4mem <- read.table(paste
                                      (path, "Extraction/Bioanalyzer_totalRNA/BioAnalyzer_CD4mem.csv",
@@ -328,7 +438,7 @@ Tot_NanoDrop_CD19 <- read.table(paste(path, "Extraction/NanoDrop_totalRNA/NanoDr
 #divide by 2.54
 path2 <- "/Users/fatimahe/Documents/Research/Epira/Lab/Extraction/Overview_analysis_in_R/Output_from_R_scripts/"
 pdf(paste(path2, "Output_overview_extraction/CD19/CD19NanoDrop.pdf", sep=""),
-    width = 29/2.54, height= 19/2.54)
+    width = 30/2.54, height= 19/2.54)
 plot(Tot_NanoDrop_CD19$X260_280 ~ 
        row.names(Tot_NanoDrop_CD19), xlab= "SampleID", ylab= "260/280", 
      main= "CD19 NanoDrop", ylim= c(1,3.5))
@@ -378,22 +488,18 @@ text(Tot_NanoDrop_CD19$X260_280[Tot_NanoDrop_CD19$SampleID== "RA5509_B"] ~
 
 
 ## Concentration for specific samples
-concentration in ng/ul for samples with 260/280 below 1.85
-text(Tot_NanoDrop_CD4naive$X260_280[Tot_NanoDrop_CD4naive$X260_280< 1.85] ~ 
-       row.names(Tot_NanoDrop_CD4naive)[Tot_NanoDrop_CD4naive$X260_280< 1.85],
-     labels= Tot_NanoDrop_CD4naive$ng_ul[Tot_NanoDrop_CD4naive$X260_280< 1.85],
-     col= 3, cex= 0.7, pos= 2)
+#concentration in ng/ul for samples with 260/280 below 1.85
+text(Tot_NanoDrop_CD19$X260_280[Tot_NanoDrop_CD19$X260_280< 1.85] ~ 
+       row.names(Tot_NanoDrop_CD19)[Tot_NanoDrop_CD19$X260_280< 1.85],
+     labels= Tot_NanoDrop_CD19$ng_ul[Tot_NanoDrop_CD19$X260_280< 1.85],
+     col= 3, cex= 0.7, pos= 3, offset= 1.3)
 
-# Concentration for samples with Samples concentration less than 20ng/ul
-text(Tot_NanoDrop_CD4naive$X260_280[Tot_NanoDrop_CD4naive$ng_ul< 20] ~ 
-       row.names(Tot_NanoDrop_CD4naive)[Tot_NanoDrop_CD4naive$ng_ul< 20],
-     labels= Tot_NanoDrop_CD4naive$ng_ul[Tot_NanoDrop_CD4naive$ng_ul< 20],
-     col= 3, cex= 0.7, pos= 2)
-# Concentration for sample RA5105_B(28_4)
-text(Tot_NanoDrop_CD4naive$X260_280[Tot_NanoDrop_CD4naive$X260_280== 1.62] ~ 
-       row.names(Tot_NanoDrop_CD4naive)[Tot_NanoDrop_CD4naive$X260_280== 1.62],
-     labels= Tot_NanoDrop_CD4naive$ng_ul[Tot_NanoDrop_CD4naive$X260_280== 1.62],
-     col= 3, cex= 0.7, pos= 2)
+#concentration in ng/ul for samples with 260/280 above 2.2
+text(Tot_NanoDrop_CD19$X260_280[Tot_NanoDrop_CD19$X260_280> 2.2] ~ 
+       row.names(Tot_NanoDrop_CD19)[Tot_NanoDrop_CD19$X260_280> 2.2],
+     labels= Tot_NanoDrop_CD19$ng_ul[Tot_NanoDrop_CD19$X260_280> 2.2],
+     col= 3, cex= 0.7, pos= 3, offset= 1.3)
+
 
 #legend
 legend("topleft", legend= c("Sample with 260/280 < 1.85",
@@ -406,6 +512,127 @@ legend("topleft", legend= c("Sample with 260/280 < 1.85",
        pt.cex= 1.2, x.intersp= 0.5, y.intersp= 0.7 )
 dev.off()
 
+
+########################## 260/280 vs conc
+path2 <- "/Users/fatimahe/Documents/Research/Epira/Lab/Extraction/Overview_analysis_in_R/Output_from_R_scripts/"
+pdf(paste(path2, "Output_overview_extraction/CD19/CD19_concVS260_280.pdf", sep=""),
+    width = 29/2.54, height= 19/2.54)
+plot(Tot_NanoDrop_CD19$X260_280, Tot_NanoDrop_CD19$ng_ul, 
+     ylab= "Concentration in ng/ul", xlab= "NanoDrop 260/280", 
+     main= "CD19 memory concentration vs 260/280")
+
+##Sample names
+# Sample names for samples with a 260/280 < 1.85
+
+#text(Tot_NanoDrop_CD19$X260_280[Tot_NanoDrop_CD19$X260_280< 1.85], 
+#     Tot_NanoDrop_CD19$ng_ul[Tot_NanoDrop_CD19$X260_280< 1.85], 
+#     Tot_NanoDrop_CD19$SampleID[Tot_NanoDrop_CD19$X260_280< 1.85],
+#     col= 2, cex= 0.7, srt= 80, adj =1.1)
+dev.off()
+
+
+
+###Plot concentration of samples
+Tot_NanoDrop_CD19_2 <- read.table(paste(path, "Extraction/NanoDrop_totalRNA/NanoDrop_CD19.csv",
+                                         sep=""), dec=",", header = T, sep=";",
+                                   stringsAsFactors = F)
+#If I would wish to sort the samples
+#plot(sort(Tot_NanoDrop_CD4naive2$ng_ul), pch= 20, ylim= c(0,130))
+#text(sort(Tot_NanoDrop_CD4naive2$ng_ul), labels= Tot_NanoDrop_CD4naive2$SampleID[order(
+#  Tot_NanoDrop_CD4naive2$ng_ul)],
+#     srt=90, cex= 0.8, pos= 3, offset= 0.8)
+
+# Mark those samples that reach a concentration above 100ng in 6 ul because this is the
+# maximum input amount in the NebNext kit
+# Create a new column with the concentrations times 6ul
+Tot_NanoDrop_CD19_2$conc_at_6_ul <- 6* Tot_NanoDrop_CD19_2$ng_ul 
+
+path2 <- "/Users/fatimahe/Documents/Research/Epira/Lab/Extraction/Overview_analysis_in_R/Output_from_R_scripts/"
+pdf(paste(path2, "Output_overview_extraction/CD19/CD19Concentration.pdf", sep=""),
+    width = 30/2.54, height= 21/2.54)
+plot(Tot_NanoDrop_CD19_2$ng_ul, pch= 20, xlab= "SampleID", ylim= c(-8, 215), 
+     ylab= "Concentration in ng/ul", main= "Concentration CD19")
+text(Tot_NanoDrop_CD19_2$ng_ul, labels= Tot_NanoDrop_CD19_2$SampleID,
+     srt=90, cex= 0.6, pos= 2, offset= 0.2, 
+     col= ifelse(Tot_NanoDrop_CD19_2$conc_at_6_ul < 100, 2, 1))
+
+text(Tot_NanoDrop_CD19_2$ng_ul[Tot_NanoDrop_CD19_2$X260_280< 1.85] ~ 
+       row.names(Tot_NanoDrop_CD19_2)[Tot_NanoDrop_CD19_2$X260_280< 1.85],
+     labels= Tot_NanoDrop_CD19_2$X260_280[Tot_NanoDrop_CD19_2$X260_280< 1.85],
+     col= 4,  srt=90, cex= 0.6, pos= 3, offset= 0.8)
+
+text(Tot_NanoDrop_CD19_2$ng_ul[Tot_NanoDrop_CD19_2$conc_at_6_ul< 100] ~ 
+       row.names(Tot_NanoDrop_CD19_2)[Tot_NanoDrop_CD19_2$conc_at_6_ul< 100],
+     labels= Tot_NanoDrop_CD19_2$X260_280[Tot_NanoDrop_CD19_2$conc_at_6_ul< 100],
+     col= 4,  srt=90, cex= 0.6, pos= 3, offset= 0.8)
+
+abline(h= mean(Tot_NanoDrop_CD19_2$ng_ul), col = "orange", lty = 4)
+# This line of code do the same as the line above:
+#abline(a= mean(Tot_NanoDrop_Cd4naive2$ng_ul), b= 0, col = 2)
+abline(a= 20, b= 0, col= "darkorange", lty =5)
+
+#Legend
+legend(x= 26, y= 220, legend= c("Sample with less than 100ng/6ul ",
+                                "Sample with more than 100ng/6ul",
+                                "260/280 value",
+                                "Mean concentration",
+                                "Min concentration for 100ng in 6ul sample"), 
+       pch= c(NA, NA, NA, NA, NA), lty= c(1, 1,1 ,4 , 5), 
+       col= c(2,1,4,"orange","darkorange"), cex= 0.8, bty= "n", 
+       pt.cex= 1.2, x.intersp= 0.5, y.intersp= 0.7 )
+
+dev.off()
+
+
+########################## BioAnalyzer
+
+Tot_BioAnalyzer_CD19 <- read.table(paste
+                                       (path, "Extraction/Bioanalyzer_totalRNA/BioAnalyzer_CD19.csv",
+                                       sep=""), dec=".", header = T, sep=";",
+                                       stringsAsFactors = F)
+
+#Plot samples to see which of the samples do not have proper RIN value
+path2 <- "/Users/fatimahe/Documents/Research/Epira/Lab/Extraction/Overview_analysis_in_R/Output_from_R_scripts/"
+pdf(paste(path2, "Output_overview_extraction/CD19/CD19BioAnalyzer.pdf", sep=""),
+    width = 29/2.54, height= 19/2.54)
+plot(Tot_BioAnalyzer_CD19$RIN, xlab= "SampleID", 
+     ylab= "RIN", main= "CD19 BioAnalyzer", ylim= c(4, 10.3))
+
+#Color sample dots if they are below a specific threshold
+points(Tot_BioAnalyzer_CD19$RIN[Tot_BioAnalyzer_CD19$RIN< 8.8] ~ 
+         row.names(Tot_BioAnalyzer_CD19)[Tot_BioAnalyzer_CD19$RIN< 8.8],
+       col= "pink", pch= 20)
+
+points(Tot_BioAnalyzer_CD19$RIN[Tot_BioAnalyzer_CD19$RIN< 8.0] ~ 
+         row.names(Tot_BioAnalyzer_CD19)[Tot_BioAnalyzer_CD19$RIN< 8.0],
+       col= 2, pch= 20)
+#Create Sample Names for pink samples
+text(Tot_BioAnalyzer_CD19$RIN[Tot_BioAnalyzer_CD19$RIN< 8.8] ~ 
+       row.names(Tot_BioAnalyzer_CD19)[Tot_BioAnalyzer_CD19$RIN<8.8],
+     labels= Tot_BioAnalyzer_CD19$SampleID[Tot_BioAnalyzer_CD19$RIN< 8.8],
+     col= "pink", cex= 0.7, srt= 80, adj =1.1)
+#Create Sample Names for red samples
+#text(Tot_BioAnalyzer_CD4naive$RIN[Tot_BioAnalyzer_CD4naive$RIN< 8.0] ~ 
+#       row.names(Tot_BioAnalyzer_CD4naive)[Tot_BioAnalyzer_CD4naive$RIN<8.0],
+#     labels= Tot_BioAnalyzer_CD4naive$SampleID[Tot_BioAnalyzer_CD4naive$RIN< 8.0],
+#     col= 2, cex= 0.7, srt= 80, adj =1.1)
+#Create RIN values for samples with RIN < 8.8
+text(Tot_BioAnalyzer_CD19$RIN[Tot_BioAnalyzer_CD19$RIN< 8.8] ~ 
+       row.names(Tot_BioAnalyzer_CD19)[Tot_BioAnalyzer_CD19$RIN<8.8],
+     labels= Tot_BioAnalyzer_CD19$RIN[Tot_BioAnalyzer_CD19$RIN< 8.8],
+     col= "blue", cex= 0.7, pos= 3)
+#Legend
+legend(x= 31, y= 4.3, legend= c("Sample with RIN < 8.8",
+                                "RIN value"), 
+       pch= c(20, NA), lty= c(NA, 1), 
+       col= c("pink",4), cex= 0.9, bty= "n", 
+       pt.cex= 1.2, x.intersp= 0.5, y.intersp= 0.7 )
+
+dev.off()
+
+
+
+sort(Tot_BioAnalyzer_CD19$SampleID)
 
 
 
